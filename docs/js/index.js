6168fa9a8a7b39e2c7808e2ed7354297f9079199
n4cl <devn4cl@gmail.com>
2018-01-08 22:49:50 +0900

    Add count up operation

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,3 +1,39 @@
 (function(){
     'use strict';
+    let g = {elm:{}};
+
+    g.elm.lbl_player1 = document.getElementById('lbl_player1');
+    g.elm.lbl_player2 = document.getElementById('lbl_player2');
+    g.elm.lbl_player3 = document.getElementById('lbl_player3');
+    g.elm.lbl_player4 = document.getElementById('lbl_player4');
+    g.elm.btn_up1 = document.getElementById('btn_up1');
+    g.elm.btn_up2 = document.getElementById('btn_up2');
+    g.elm.btn_up3 = document.getElementById('btn_up3');
+    g.elm.btn_up4 = document.getElementById('btn_up4');
+    g.elm.btn_down1 = document.getElementById('btn_up1');
+    g.elm.btn_down2 = document.getElementById('btn_up2');
+    g.elm.btn_down3 = document.getElementById('btn_up3');
+    g.elm.btn_down4 = document.getElementById('btn_up4');
+
+    g.elm.btn_up1.onclick = function () {
+        g.elm.lbl_player1.firstChild.nodeValue = upCount(g.elm.lbl_player1.firstChild.nodeValue);
+    };
+    g.elm.btn_up2.onclick = function () {
+        g.elm.lbl_player2.firstChild.nodeValue = upCount(g.elm.lbl_player2.firstChild.nodeValue);
+    };
+    g.elm.btn_up3.onclick = function () {
+        g.elm.lbl_player3.firstChild.nodeValue = upCount(g.elm.lbl_player3.firstChild.nodeValue);
+    };
+    g.elm.btn_up4.onclick = function () {
+        g.elm.lbl_player4.firstChild.nodeValue = upCount(g.elm.lbl_player4.firstChild.nodeValue);
+    };
+
+    function upCount(nodeValue) {
+        let v = parseInt(nodeValue, 10);
+        if (v < 3) {
+            v += 1;
+        }
+        return v;
+    }
+
 })();
